34487ccca79783bf4c0345c4d0e27a61c318bed9
corranrogue9 <corranrogue9@gmail.com>
2016-03-26 11:22:49 -0700

    added a script to build all architectures and configurations into a single package directory

---
+++ Scripts/build.cmd
@@ -0,0 +1,4 @@
+set reporoot=%1
+set PackagePath=%2
+
+powershell.exe -executionpolicy remotesigned -File %~dp0build.ps1 %reporoot% %PackagePath%
---
+++ Scripts/build.ps1
@@ -0,0 +1,21 @@
+$repoRoot = [System.IO.Path]::GetFullPath($args[0])
+$packagePath = [System.IO.Path]::GetFullPath($args[1])
+
+$configurations = "Debug","Release"
+$architectures = "AnyCPU","x86","x64"
+$frameworks = "NET30","NET35","NET40","NET45","NET46"
+
+ForEach ($configuration in $configurations)
+{
+    ForEach ($architecture in $architectures)
+    {
+        ForEach ($framework in $frameworks)
+        {
+            $output = Invoke-Expression "msbuild $repoRoot /t:Clean /t:Rebuild /property:Configuration=$configuration /property:Platform=$architecture.$framework 2>&1"
+            if ($LASTEXITCODE -ne 0)
+            {
+                $output
+            }
+        }
+    }
+}
