24cced08ec181ec5473c2863f3c82fa6d6f97a19
corranrogue9 <corranrogue9@gmail.com>
2016-04-26 15:12:11 -0700

    removed building against 64-bit architectures

--- Scripts/build.ps1
+++ Scripts/build.ps1
@@ -2,7 +2,7 @@ $repoRoot = [System.IO.Path]::GetFullPath($args[0])
 $packagePath = [System.IO.Path]::GetFullPath($args[1])
 
 $configurations = "Debug","Release"
-$architectures = "AnyCPU","x86","x64"
+$architectures = "AnyCPU","x86"
 $frameworks = "NET30","NET35","NET40","NET45","NET46"
 
 ForEach ($configuration in $configurations)
