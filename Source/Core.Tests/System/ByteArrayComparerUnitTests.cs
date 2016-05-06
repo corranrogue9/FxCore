c196e05cf724521142b008c06953177855f17b64
corranrogue9 <corranrogue9@gmail.com>
2016-05-05 19:16:11 -0700

    added a IEqualityComparer implementation for comparing byte arrays for equality; closes #27

---
+++ Source/Core.Tests/System/ByteArrayComparerUnitTests.cs
@@ -0,0 +1,144 @@
+﻿namespace System
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Unit tests for the <see cref="ByteArrayComparer"/>
+    /// </summary>
+    /// <threadsafety static="true" instance="true"/>
+    [TestClass]
+    public sealed class ByteArrayComparerUnitTests
+    {
+        /// <summary>
+        /// Compares two null byte arrays for equality
+        /// </summary>
+        [TestCategory("Unit")]
+        [Priority(2)]
+        [Description("Compares two null byte arrays for equality")]
+        [TestMethod]
+        public void EqualsNulls()
+        {
+            byte[] value1 = null;
+            byte[] value2 = null;
+            Assert.IsTrue(ByteArrayComparer.Instance.Equals(value1, value2));
+        }
+
+        /// <summary>
+        /// Compares a null byte array with a non-null byte array for equality
+        /// </summary>
+        [TestCategory("Unit")]
+        [Priority(2)]
+        [Description("Compares a null byte array with a non-null byte array for equality")]
+        [TestMethod]
+        public void EqualsNull()
+        {
+            byte[] value1 = null;
+            var value2 = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+            Assert.IsFalse(ByteArrayComparer.Instance.Equals(value1, value2));
+
+            var value3 = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+            byte[] value4 = null;
+            Assert.IsFalse(ByteArrayComparer.Instance.Equals(value3, value4));
+        }
+
+        /// <summary>
+        /// Compares two byte arrays that have different lengths for equality
+        /// </summary>
+        [TestCategory("Unit")]
+        [Priority(2)]
+        [Description("Compares two byte arrays that have different lengths for equality")]
+        [TestMethod]
+        public void EqualsDifferentLengths()
+        {
+            var value1 = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 };
+            var value2 = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+            Assert.IsFalse(ByteArrayComparer.Instance.Equals(value1, value2));
+        }
+
+        /// <summary>
+        /// Compares two arrays that are the same for equality
+        /// </summary>
+        [TestCategory("Unit")]
+        [Priority(2)]
+        [Description("Compares two arrays that are the same for equality")]
+        [TestMethod]
+        public void EqualsSameArrays()
+        {
+            var value1 = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+            var value2 = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+            Assert.IsTrue(ByteArrayComparer.Instance.Equals(value1, value2));
+            Assert.AreEqual(ByteArrayComparer.Instance.GetHashCode(value1), ByteArrayComparer.Instance.GetHashCode(value2));
+        }
+
+        /// <summary>
+        /// Compares for equality two arrays that are the same, and then compares each with a third, identical array
+        /// </summary>
+        [TestCategory("Unit")]
+        [Priority(2)]
+        [Description("Compares for equality two arrays that are the same, and then compares each with a third, identical array")]
+        [TestMethod]
+        public void EqualityTransitive()
+        {
+            var value1 = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+            var value2 = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+            var value3 = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+
+            Assert.IsTrue(ByteArrayComparer.Instance.Equals(value1, value2));
+            Assert.AreEqual(ByteArrayComparer.Instance.GetHashCode(value1), ByteArrayComparer.Instance.GetHashCode(value2));
+
+            Assert.IsTrue(ByteArrayComparer.Instance.Equals(value2, value3));
+            Assert.AreEqual(ByteArrayComparer.Instance.GetHashCode(value2), ByteArrayComparer.Instance.GetHashCode(value3));
+
+            Assert.IsTrue(ByteArrayComparer.Instance.Equals(value1, value3));
+            Assert.AreEqual(ByteArrayComparer.Instance.GetHashCode(value1), ByteArrayComparer.Instance.GetHashCode(value3));
+        }
+
+        /// <summary>
+        /// Compares for equality two arrays that are the same, and then compares them swapping order
+        /// </summary>
+        [TestCategory("Unit")]
+        [Priority(2)]
+        [Description("Compares for equality two arrays that are the same, and then compares them swapping order")]
+        [TestMethod]
+        public void EqualitySymmetric()
+        {
+            var value1 = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+            var value2 = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+
+            Assert.IsTrue(ByteArrayComparer.Instance.Equals(value1, value2));
+            Assert.AreEqual(ByteArrayComparer.Instance.GetHashCode(value1), ByteArrayComparer.Instance.GetHashCode(value2));
+
+            Assert.IsTrue(ByteArrayComparer.Instance.Equals(value2, value1));
+            Assert.AreEqual(ByteArrayComparer.Instance.GetHashCode(value2), ByteArrayComparer.Instance.GetHashCode(value1));
+        }
+
+        /// <summary>
+        /// Compares an array with itself for equality
+        /// </summary>
+        [TestCategory("Unit")]
+        [Priority(2)]
+        [Description("Compares an array with itself for equality")]
+        [TestMethod]
+        public void EqualityReflexive()
+        {
+            var value = new byte[] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 };
+            Assert.IsTrue(ByteArrayComparer.Instance.Equals(value, value));
+            Assert.AreEqual(ByteArrayComparer.Instance.GetHashCode(value), ByteArrayComparer.Instance.GetHashCode(value));
+        }
+
+        /// <summary>
+        /// Compares two empty byte arrays for equality
+        /// </summary>
+        [TestCategory("Unit")]
+        [Priority(2)]
+        [Description("Compares two empty byte arrays for equality")]
+        [TestMethod]
+        public void EqualityEmptyArray()
+        {
+            var value1 = new byte[0];
+            var value2 = new byte[0];
+            Assert.IsTrue(ByteArrayComparer.Instance.Equals(value1, value2));
+            Assert.AreEqual(ByteArrayComparer.Instance.GetHashCode(value1), ByteArrayComparer.Instance.GetHashCode(value2));
+        }
+    }
+}
---
+++ Source/Core/System/ByteArrayComparer.cs
@@ -0,0 +1,97 @@
+﻿namespace System
+{
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+    using System.Security;
+
+    using Fx;
+
+    /// <summary>
+    /// A <see cref="IEqualityComparer{T}"/> that compares <see cref="T:byte[]"/>s for equality
+    /// </summary>
+    /// <threadsafety static="true" instance="true"/>
+    public sealed class ByteArrayComparer : IEqualityComparer<byte[]>
+    {
+        /// <summary>
+        /// A singleton instance of the <see cref="ByteArrayComparer"/>
+        /// </summary>
+        private static readonly ByteArrayComparer Singleton = new ByteArrayComparer();
+        
+        /// <summary>
+        /// Prevents a default instance of the <see cref="ByteArrayComparer"/> class from being created
+        /// </summary>
+        private ByteArrayComparer()
+        {
+        }
+
+        /// <summary>
+        /// Gets a singleton instance of the <see cref="ByteArrayComparer"/>
+        /// </summary>
+        public static ByteArrayComparer Instance
+        {
+            get
+            {
+                return Singleton;
+            }
+        }
+
+        /// <summary>
+        /// Determines whether or not the specified objects are equals
+        /// </summary>
+        /// <param name="x">The first object of type <see cref="T:byte[]"/> to compare</param>
+        /// <param name="y">The second object of type <see cref="T:byte[]"/> to compare</param>
+        /// <returns>true if the specified objects are equal; otherwise, false</returns>
+        public bool Equals(byte[] x, byte[] y)
+        {
+            if (x == y)
+            {
+                return true;
+            }
+
+            if (x == null || y == null)
+            {
+                return false;
+            }
+
+            if (x.Length != y.Length)
+            {
+                return false;
+            }
+
+            return SafeNativeMethods.memcmp(x, y, new UIntPtr(Convert.ToUInt32(x.Length))) == 0;
+        }
+
+        /// <summary>
+        /// Returns a hash code for the specified object
+        /// </summary>
+        /// <param name="obj">The <see cref="object"/> for which a hash code is to be returned</param>
+        /// <returns>A hash code for the specified object</returns>
+        /// <exception cref="ArgumentNullException">The type of <paramref name="obj"/> is a reference type and <paramref name="obj"/> is null</exception>
+        public int GetHashCode(byte[] obj)
+        {
+            Ensure.NotNull(obj, nameof(obj));
+
+            var result = 0;
+            for (int i = 0; i < obj.Length; ++i)
+            {
+                result ^= obj[i];
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Unmanaged methods that are used for interacting with byte arrays in memory
+        /// </summary>
+        [SuppressUnmanagedCodeSecurity]
+        private static class SafeNativeMethods
+        {
+#if !NET40
+            // this message is a bug in pre-.NET 4.0 versions
+            [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Security", "CA5122:PInvokesShouldNotBeSafeCriticalFxCopRule")]
+#endif
+            [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
+            public static extern int memcmp(byte[] ptr1, byte[] ptr2, UIntPtr num);
+        }
+    }
+}
