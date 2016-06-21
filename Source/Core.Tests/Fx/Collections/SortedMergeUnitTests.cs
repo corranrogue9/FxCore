8040b92418dac83e47d587050b48e7d85b188932
Christof Sprenger <christof_sprenger@hotmail.com>
2016-06-20 21:09:26 -0700

    add two missing files from previos commit

---
+++ Source/Core.Tests/Fx/Collections/SortedMergeUnitTests.cs
@@ -0,0 +1,39 @@
+﻿namespace Fx
+{
+    using System;
+    using System.Linq;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Collections.Generic;
+    using Collections;
+
+    /// <summary>
+    /// Unit tests for the <see cref="Ensure"/>
+    /// </summary>
+    /// <threadsafety static="true" instance="true"/>
+    [TestClass]
+    public sealed class SortedMergeUnitTests
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestCategory("Unit")]
+        [Description("Ensures that a simple sample data set gets sorted")]
+        [Priority(1)]
+        [TestMethod]
+        public void SampleDataTest1()
+
+        {
+            ICollection<IEnumerable<int>> x = new List<IEnumerable<int>>
+            {
+                new List<int> { 5, 7, 9 },
+                new List<int> { 1, 2 ,3 },
+                new List<int> { 4, 6, 8 }
+            };
+
+            var actual = x.Sorted(Comparer<int>.Default).ToList();
+       
+            CollectionAssert.AreEqual(new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, actual);
+        }
+    }
+}
---
+++ Source/Core/Fx/Collections/SortedPartitionsMergeExtension.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fx.Collections
+{
+    /// <summary>
+    /// Extension call to merge sorted enumerables into a single sorted enumerable
+    ///
+    /// </summary>
+    public static class SortedPartitionsMergeExtension
+    {
+        /// <summary>
+        /// merge the given enumerables that have to be sorted into a single sorted enumerable
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumerables">a collection of enumerables each already sorted (in repsect to comparer></param>
+        /// <param name="comparer"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Sorted<T>(this ICollection<IEnumerable<T>> enumerables, Comparer<T> comparer = null)
+        {
+            var enumeratorComparer = new EnumeratorComparer<T>(comparer ?? Comparer<T>.Default);
+            var enumerators = GetEnumerators(enumerables, enumeratorComparer);
+
+            while (enumerators.Count > 0)
+            {
+                var top = enumerators.First();
+                enumerators.Remove(top);
+
+                yield return top.Current;
+
+                if (top.MoveNext())
+                {
+                    var ix = enumerators.BinarySearch(top, enumeratorComparer);
+                    var i = ix < 0 ? ~ix : ix;
+
+                    enumerators.Insert(i, top);
+                }
+                else
+                {
+                    top.Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// get the list of enumerators of all the non empty Enumerables, sorted by their first (current) item
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumerables"></param>
+        /// <returns></returns>
+        private static List<IEnumerator<T>> GetEnumerators<T>(ICollection<IEnumerable<T>> enumerables, EnumeratorComparer<T> enumeratorComparer)
+        {
+            var enumerators = new List<IEnumerator<T>>(enumerables.Count);
+            foreach (var enumerable in enumerables)
+            {
+                var e = enumerable.GetEnumerator();
+                if (e.MoveNext())
+                {
+                    enumerators.Add(e);
+                }
+                else
+                {
+                    e.Dispose();
+                }
+            }
+            enumerators.Sort(enumeratorComparer);
+            return enumerators;
+        }
+    }
+
+    class EnumeratorComparer<T> : IComparer<IEnumerator<T>>
+    {
+        private readonly IComparer<T> _comparer;
+
+        public EnumeratorComparer(IComparer<T> comparer)
+        {
+            _comparer = comparer;
+        }
+
+        public int Compare(IEnumerator<T> x, IEnumerator<T> y)
+        {
+            return _comparer.Compare(x.Current, y.Current);
+        }
+    }
+}
