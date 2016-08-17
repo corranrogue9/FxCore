627a489a9624f854df2b9c6f6cc9928eb3c886e7
corranrogue9 <corranrogue9@gmail.com>
2016-08-17 12:42:46 -0700

    added an abstraction for serializing and deserializing objects; issue #17

---
+++ Source/Core.Tests/Fx/Serialization/MalformedSerializableType.cs
@@ -0,0 +1,49 @@
+﻿namespace Fx.Serialization
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// A mock type that is not serializable to be used for testing
+    /// </summary>
+    /// <threadsafety static="true" instance="true"/>
+    [DataContract(Name = "MalformedSerializableType")]
+    public sealed class MalformedSerializableType
+    {
+        /// <summary>
+        /// A singleton instance of the <see cref="MalformedSerializableType"/>
+        /// </summary>
+        private static readonly MalformedSerializableType Singleton = new Func<MalformedSerializableType>(() =>
+        {
+            var first = new MalformedSerializableType();
+            var second = new MalformedSerializableType();
+            first.Data = second;
+            second.Data = first;
+            return first;
+        }).Invoke();
+
+        /// <summary>
+        /// Prevents a default instance of the <see cref="MalformedSerializableType"/> class from being created
+        /// </summary>
+        private MalformedSerializableType()
+        {
+        }
+
+        /// <summary>
+        /// Gets a singleton instance of the <see cref="MalformedSerializableType"/>
+        /// </summary>
+        public static MalformedSerializableType Instance
+        {
+            get
+            {
+                return Singleton;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets <see cref="MalformedSerializableType"/> that represents a nested data type
+        /// </summary>
+        [DataMember(Name = "Data", IsRequired = true)]
+        public MalformedSerializableType Data { get; set; }
+    }
+}
---
+++ Source/Core/Fx/Serialization/ISerializer.cs
@@ -0,0 +1,71 @@
+﻿namespace Fx.Serialization
+{
+    using System.IO;
+
+    /// <summary>
+    /// Serializes objects into different representations and deserializes those representations into objects
+    /// </summary>
+    /// <threadsafety static="true" instance="true"/>
+    public interface ISerializer
+    {
+        /// <summary>
+        /// Serializes <paramref name="toSerialize"/> into its <see cref="string"/> representation
+        /// </summary>
+        /// <param name="toSerialize">The object to serialize</param>
+        /// <returns>The <see cref="string"/> representation of <paramref name="toSerialize"/></returns>
+        /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="toSerialize"/> is null</exception>
+        /// <exception cref="System.Runtime.Serialization.SerializationException">Thrown if an error occured while serializing <paramref name="toSerialize"/></exception>
+        /// <typeparam name="T">The type of the object be serialized</typeparam>
+        string ToString<T>(T toSerialize);
+
+        /// <summary>
+        /// Serializes <paramref name="toSerialize"/> into its <see cref="T:byte[]"/> representation
+        /// </summary>
+        /// <param name="toSerialize">The object to serialize</param>
+        /// <returns>The <see cref="T:byte[]"/> representation of <paramref name="toSerialize"/></returns>
+        /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="toSerialize"/> is null</exception>
+        /// <exception cref="System.Runtime.Serialization.SerializationException">Thrown if an error occured while serializing <paramref name="toSerialize"/></exception>
+        /// <typeparam name="T">The type of the object be serialized</typeparam>
+        byte[] ToBytes<T>(T toSerialize);
+
+        /// <summary>
+        /// Serializes <paramref name="toSerialize"/> into its <see cref="Stream"/> representation
+        /// </summary>
+        /// <param name="toSerialize">The object to serialize</param>
+        /// <returns>The <see cref="Stream"/> representation of <paramref name="toSerialize"/></returns>
+        /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="toSerialize"/> is null</exception>
+        /// <exception cref="System.Runtime.Serialization.SerializationException">Thrown if an error occured while serializing <paramref name="toSerialize"/></exception>
+        /// <typeparam name="T">The type of the object be serialized</typeparam>
+        Stream ToStream<T>(T toSerialize);
+
+        /// <summary>
+        /// Deserializes <paramref name="toDeserialize"/> into the object that it represents
+        /// </summary>
+        /// <param name="toDeserialize">The <see cref="string"/> to deserialize</param>
+        /// <returns>The object represented by <paramref name="toDeserialize"/></returns>
+        /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="toDeserialize"/> is null</exception>
+        /// <exception cref="System.Runtime.Serialization.SerializationException">Thrown if an error occurred while deserializing <paramref name="toDeserialize"/></exception>
+        /// <typeparam name="T">The type of the object be deserialized</typeparam>
+        T FromString<T>(string toDeserialize);
+
+        /// <summary>
+        /// Deserializes <paramref name="toDeserialize"/> into the object that it represents
+        /// </summary>
+        /// <param name="toDeserialize">The <see cref="T:byte[]"/> to deserialize</param>
+        /// <returns>The object represented by <paramref name="toDeserialize"/></returns>
+        /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="toDeserialize"/> is null</exception>
+        /// <exception cref="System.Runtime.Serialization.SerializationException">Thrown if an error occurred while deserializing <paramref name="toDeserialize"/></exception>
+        /// <typeparam name="T">The type of the object be deserialized</typeparam>
+        T FromBytes<T>(byte[] toDeserialize);
+
+        /// <summary>
+        /// Deserializes <paramref name="toDeserialize"/> into the object that it represents
+        /// </summary>
+        /// <param name="toDeserialize">The <see cref="Stream"/> to deserialize</param>
+        /// <returns>The object represented by <paramref name="toDeserialize"/></returns>
+        /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="toDeserialize"/> is null</exception>
+        /// <exception cref="System.Runtime.Serialization.SerializationException">Thrown if an error occurred while deserializing <paramref name="toDeserialize"/></exception>
+        /// <typeparam name="T">The type of the object be deserialized</typeparam>
+        T FromStream<T>(Stream toDeserialize);
+    }
+}
