f135667d7e2f4110cc527309589f285dfc5f2fa4
corranrogue9 <corranrogue9@gmail.com>
2016-08-30 12:31:23 -0700

    added a ISerializer implementation which leverages the .NET binary formatter API; issue #17

---
+++ Source/Core.Tests/Fx/Serialization/BinarySerializerFailureTests.cs
@@ -0,0 +1,156 @@
+﻿namespace Fx.Serialization
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Failure tests for the <see cref="BinarySerializer"/>
+    /// </summary>
+    /// <threadsafety static="true" instance="true"/>
+    [TestClass]
+    public sealed class BinarySerializerFailureTests
+    {
+        /// <summary>
+        /// Attempts to serialize a null object to bytes
+        /// </summary>
+        [TestCategory("Failure")]
+        [Description("Attempts to serialize a null object to bytes")]
+        [Priority(1)]
+        [TestMethod]
+        public void SerializeNullBytes()
+        {
+            SerializerFailureTests.SerializeNullBytes(BinarySerializer.Default);
+        }
+
+        /// <summary>
+        /// Attempts to serialize a null object to a string
+        /// </summary>
+        [TestCategory("Failure")]
+        [Description("Attempts to serialize a null object to a string")]
+        [Priority(1)]
+        [TestMethod]
+        public void SerializeNullString()
+        {
+            SerializerFailureTests.SerializeNullString(BinarySerializer.Default);
+        }
+
+        /// <summary>
+        /// Attempts to serialize a null object to a stream
+        /// </summary>
+        [TestCategory("Failure")]
+        [Description("Attempts to serialize a null object to a stream")]
+        [Priority(1)]
+        [TestMethod]
+        public void SerializeNullStream()
+        {
+            SerializerFailureTests.SerializeNullStream(BinarySerializer.Default);
+        }
+
+        /// <summary>
+        /// Attempts to deserialize null bytes
+        /// </summary>
+        [TestCategory("Failure")]
+        [Description("Attempts to deserialize null bytes")]
+        [Priority(1)]
+        [TestMethod]
+        public void DeserializeNullBytes()
+        {
+            SerializerFailureTests.DeserializeNullBytes(BinarySerializer.Default);
+        }
+
+        /// <summary>
+        /// Attempts to deserialize a null string
+        /// </summary>
+        [TestCategory("Failure")]
+        [Description("Attempts to deserialize a null string")]
+        [Priority(1)]
+        [TestMethod]
+        public void DeserializeNullString()
+        {
+            SerializerFailureTests.DeserializeNullString(BinarySerializer.Default);
+        }
+
+        /// <summary>
+        /// Attempts to deserialize a null stream
+        /// </summary>
+        [TestCategory("Failure")]
+        [Description("Attempts to deserialize a null stream")]
+        [Priority(1)]
+        [TestMethod]
+        public void DeserializeNullStream()
+        {
+            SerializerFailureTests.DeserializeNullStream(BinarySerializer.Default);
+        }
+
+        /// <summary>
+        /// Attempts to serialize a non-serializable object to a stream
+        /// </summary>
+        [TestCategory("Failure")]
+        [Description("Attempts to serialize a non-serializable object to a stream")]
+        [Priority(1)]
+        [TestMethod]
+        public void SerializeMalformedTypeStream()
+        {
+            SerializerFailureTests.SerializeMalformedTypeStream(BinarySerializer.Default);
+        }
+
+        /// <summary>
+        /// Attempts to serialize a non-serializable object to bytes
+        /// </summary>
+        [TestCategory("Failure")]
+        [Description("Attempts to serialize a non-serializable object to bytes")]
+        [Priority(1)]
+        [TestMethod]
+        public void SerializeMalformedTypeBytes()
+        {
+            SerializerFailureTests.SerializeMalformedTypeBytes(BinarySerializer.Default);
+        }
+
+        /// <summary>
+        /// Attempts to serialize a non-serializable object to a string
+        /// </summary>
+        [TestCategory("Failure")]
+        [Description("Attempts to serialize a non-serializable object to a string")]
+        [Priority(1)]
+        [TestMethod]
+        public void SerializeMalformedTypeString()
+        {
+            SerializerFailureTests.SerializeMalformedTypeString(BinarySerializer.Default);
+        }
+
+        /// <summary>
+        /// Attempts to deserialize a malformed stream
+        /// </summary>
+        [TestCategory("Failure")]
+        [Description("Attempts to deserialize a malformed stream")]
+        [Priority(1)]
+        [TestMethod]
+        public void DeserializeMalformedTypeStream()
+        {
+            SerializerFailureTests.DeserializeMalformedTypeStream(BinarySerializer.Default);
+        }
+
+        /// <summary>
+        /// Attempts to deserialize malformed bytes
+        /// </summary>
+        [TestCategory("Failure")]
+        [Description("Attempts to deserialize malformed bytes")]
+        [Priority(1)]
+        [TestMethod]
+        public void DeserializeMalformedTypeBytes()
+        {
+            SerializerFailureTests.DeserializeMalformedTypeBytes(BinarySerializer.Default);
+        }
+
+        /// <summary>
+        /// Attempts to deserialize a malformed string
+        /// </summary>
+        [TestCategory("Failure")]
+        [Description("Attempts to deserialize a malformed string")]
+        [Priority(1)]
+        [TestMethod]
+        public void DeserializeMalformedTypeString()
+        {
+            SerializerFailureTests.DeserializeMalformedTypeString(BinarySerializer.Default);
+        }
+    }
+}
---
+++ Source/Core.Tests/Fx/Serialization/BinarySerializerUnitTests.cs
@@ -0,0 +1,60 @@
+﻿namespace Fx.Serialization
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Unit tests for the <see cref="BinarySerializer"/>
+    /// </summary>
+    /// <threadsafety static="true" instance="true"/>
+    [TestClass]
+    public sealed class BinarySerializerUnitTests
+    {
+        /// <summary>
+        /// Serializes and deserializes an object to bytes and asserts that they are equivalent
+        /// </summary>
+        [TestCategory("Unit")]
+        [Description("Serializes and deserializes an object to bytes and asserts that they are equivalent")]
+        [Priority(1)]
+        [TestMethod]
+        public void SerializeBytes()
+        {
+            SerializerUnitTests.SerializeBytes(BinarySerializer.Default);
+        }
+
+        /// <summary>
+        /// Serializes and deserializes an object to a stream and asserts that they are equivalent
+        /// </summary>
+        [TestCategory("Unit")]
+        [Description("Serializes and deserializes an object to a stream and asserts that they are equivalent")]
+        [Priority(1)]
+        [TestMethod]
+        public void SerializeStream()
+        {
+            SerializerUnitTests.SerializeStream(BinarySerializer.Default);
+        }
+
+        /// <summary>
+        /// Serializes and deserializes an object to a string and asserts that they are equivalent
+        /// </summary>
+        [TestCategory("Unit")]
+        [Description("Serializes and deserializes an object to a string and asserts that they are equivalent")]
+        [Priority(1)]
+        [TestMethod]
+        public void SerializeString()
+        {
+            SerializerUnitTests.SerializeString(BinarySerializer.Default);
+        }
+
+        /// <summary>
+        /// Serializes and deserializes a string and asserts that they are equivalent
+        /// </summary>
+        [TestCategory("Unit")]
+        [Description("Serializes and deserializes a string and asserts that they are equivalent")]
+        [Priority(1)]
+        [TestMethod]
+        public void StringSerialization()
+        {
+            SerializerUnitTests.StringSerialization(BinarySerializer.Default);
+        }
+    }
+}
