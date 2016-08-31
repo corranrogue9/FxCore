71bdf3d77ab554931d682157cb5af93e0bcad64a
corranrogue9 <corranrogue9@gmail.com>
2016-08-30 21:00:09 -0700

    created a IContextProvider abstraction for adding metadata to the current logical operation; issue #14

---
+++ Source/Core.Tests/Fx/ContextProvision/ContextProviderUnitTests.cs
@@ -0,0 +1,74 @@
+﻿namespace Fx.ContextProvision
+{
+    using System.Collections.Generic;
+    
+    /// <summary>
+    /// Unit tests for the <see cref="IContextProvider"/> abstraction
+    /// </summary>
+    /// <threadsafety static="true"/>
+    internal static class ContextProviderUnitTests
+    {
+        /// <summary>
+        /// Provides a null collection to a context provider
+        /// </summary>
+        /// <param name="contextProvider">The <see cref="IContextProvider"/> to text</param>
+        /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="contextProvider"/> is null</exception>
+        public static void ProvideNullContext(IContextProvider contextProvider)
+        {
+            Ensure.NotNull(contextProvider, nameof(contextProvider));
+
+            contextProvider.ProvideContext(null).Dispose();
+        }
+
+        /// <summary>
+        /// Provides a collection with a null key to a context provider
+        /// </summary>
+        /// <param name="contextProvider">The <see cref="IContextProvider"/> to text</param>
+        /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="contextProvider"/> is null</exception>
+        public static void ProvideNullKey(IContextProvider contextProvider)
+        {
+            Ensure.NotNull(contextProvider, nameof(contextProvider));
+
+            contextProvider.ProvideContext(new[] { new KeyValuePair<string, object>(null, new object()) }).Dispose();
+        }
+
+        /// <summary>
+        /// Provides a collection with a null value to a context provider
+        /// </summary>
+        /// <param name="contextProvider">The <see cref="IContextProvider"/> to text</param>
+        /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="contextProvider"/> is null</exception>
+        public static void ProvideNullValue(IContextProvider contextProvider)
+        {
+            Ensure.NotNull(contextProvider, nameof(contextProvider));
+
+            contextProvider.ProvideContext(new[] { new KeyValuePair<string, object>("key", null) }).Dispose();
+        }
+
+        /// <summary>
+        /// Provides a collection with a null key and value to a context provider
+        /// </summary>
+        /// <param name="contextProvider">The <see cref="IContextProvider"/> to text</param>
+        /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="contextProvider"/> is null</exception>
+        public static void ProvideNullDatum(IContextProvider contextProvider)
+        {
+            Ensure.NotNull(contextProvider, nameof(contextProvider));
+
+            contextProvider.ProvideContext(new[] { new KeyValuePair<string, object>(null, null) }).Dispose();
+        }
+
+        /// <summary>
+        /// Provides metadata to a context provider and disposes the context multiple times
+        /// </summary>
+        /// <param name="contextProvider">The <see cref="IContextProvider"/> to text</param>
+        /// <exception cref="System.ArgumentNullException">Thrown if <paramref name="contextProvider"/> is null</exception>
+        public static void DisposeContext(IContextProvider contextProvider)
+        {
+            Ensure.NotNull(contextProvider, nameof(contextProvider));
+
+            using (var context = contextProvider.ProvideContext(new[] { new KeyValuePair<string, object>("asdf", new object()) }))
+            {
+                context.Dispose();
+            }
+        }
+    }
+}
---
+++ Source/Core/Fx/ContextProvision/IContextProvider.cs
@@ -0,0 +1,20 @@
+﻿namespace Fx.ContextProvision
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Provides contextual data for tracing events
+    /// </summary>
+    /// <threadsafety instance="true"/>
+    public interface IContextProvider
+    {
+        /// <summary>
+        /// Adds contextual metadata represented by <paramref name="data"/> to the current logical operation
+        /// </summary>
+        /// <param name="data">The metadata that should be added to the current logical operation</param>
+        /// <returns>The new context containing all of the current logical operation information; when disposed, this object will remove the newly added metadata from the current logical operation</returns>
+        /// <remarks>Due to the nature of this functionality being critical to instrumentation, in the error case of <paramref name="data"/> being null, it should be assumed that data is an empty collection. Further, in the event that any key or value within the collection is null, the case should be handled without throwing <see cref="Exception"/>s</remarks>
+        IDisposable ProvideContext(IEnumerable<KeyValuePair<string, object>> data);
+    }
+}
