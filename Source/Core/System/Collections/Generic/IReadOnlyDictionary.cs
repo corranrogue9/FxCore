5c7b4f696e4bf1ae9cf224c58769407ac0fb4b14
corranrogue9 <corranrogue9@gmail.com>
2016-10-25 14:32:05 -0700

    added read-only dictionary interface for .NET versions prior to 4.5; issue #63

---
+++ Source/Core/System/Collections/Generic/IReadOnlyDictionary.cs
@@ -0,0 +1,52 @@
+﻿#if !NET45
+namespace System.Collections.Generic
+{
+    /// <summary>
+    /// Represents a generic read-only collection of key/value pairs
+    /// </summary>
+    /// <typeparam name="TKey">The type of keys in the read-only dictionary</typeparam>
+    /// <typeparam name="TValue">The type of values in the read-only dictionary</typeparam>
+    public interface IReadOnlyDictionary<TKey, TValue> : IReadOnlyCollection<KeyValuePair<TKey, TValue>>, IEnumerable<KeyValuePair<TKey, TValue>>, IEnumerable
+    {
+        /// <summary>
+        /// Gets an enumerable collection that contains the keys in the read-only dictionary
+        /// </summary>
+        IEnumerable<TKey> Keys { get; }
+
+        /// <summary>
+        /// Gets an enumerable collection that contains the values in the read-only dictionary
+        /// </summary>
+        IEnumerable<TValue> Values { get; }
+
+        /// <summary>
+        /// Gets the element that has the specified key in the read-only dictionary
+        /// </summary>
+        /// <param name="key">The key to locate</param>
+        /// <returns>The element that has the specified key in the read-only dictionary</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="key"/> is null</exception>
+        /// <exception cref="KeyNotFoundException">Thrown if the property is retrieved and <paramref name="key"/> is not found</exception>
+        TValue this[TKey key] { get; }
+
+        /// <summary>
+        /// Determines whether the read-only dictionary contains an element that has the specified key
+        /// </summary>
+        /// <param name="key">The key to locate</param>
+        /// <returns>true if the read-only dictionary contains an element that has the specified key; otherwise, false</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="key"/> is null</exception>
+        bool ContainsKey(TKey key);
+
+        /// <summary>
+        /// Gets the value that is associated with the specified key
+        /// </summary>
+        /// <param name="key">The key to locate</param>
+        /// <param name="value">
+        /// When this method returns, the value associated with the specified key, if the key is found; otherwise, the default value for the type of the value parameter. This parameter is passed uninitialized
+        /// </param>
+        /// <returns>
+        /// true if the object that implements the <see cref="IReadOnlyDictionary{TKey, TValue}"/> interface contains an element that has the specified key; otherwise, false
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="key"/> is null</exception>
+        bool TryGetValue(TKey key, out TValue value);
+    }
+}
+#endif
