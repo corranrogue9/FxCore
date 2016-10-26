b12dd979888b523452846184a7dfbb6d514e70b8
corranrogue9 <corranrogue9@gmail.com>
2016-10-26 00:44:37 -0700

    added a read-only collection adapter for .NET versions prior to 4.6; issue #63

---
+++ Source/Core.Tests/System/Collections/Generic/ReadOnlyCollectionUnitTests.cs
@@ -0,0 +1,98 @@
+﻿namespace System.Collections.Generic
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Unit tests for the <see cref="ReadOnlyCollection{T}"/>
+    /// </summary>
+    /// <threadsafety static="true" instance="true"/>
+    [TestClass]
+    public sealed class ReadOnlyCollectionUnitTests
+    {
+        /// <summary>
+        /// Ensures that a readonly collection has the same count as its delegate
+        /// </summary>
+        [TestCategory("Unit")]
+        [Priority(2)]
+        [Description("Ensures that a readonly collection has the same count as its delegate")]
+        [TestMethod]
+        public void CollectionCount()
+        {
+            var collection = new List<string>();
+            var readonlyCollection = new ReadOnlyCollection<string>(collection);
+
+            Assert.AreEqual(collection.Count, readonlyCollection.Count);
+
+            collection.Add("hello");
+            Assert.AreEqual(collection.Count, readonlyCollection.Count);
+
+            collection.Add("hello");
+            Assert.AreEqual(collection.Count, readonlyCollection.Count);
+
+            collection.Add("hello");
+            Assert.AreEqual(collection.Count, readonlyCollection.Count);
+        }
+
+        /// <summary>
+        /// Ensures that enumerating a readonly collection has the same results as enumerating into its delegate
+        /// </summary>
+        [TestCategory("Unit")]
+        [Priority(2)]
+        [Description("Ensures that enumerating a readonly collection has the same results as enumerating into its delegate")]
+        [TestMethod]
+        public void CollectionGenericEnumeration()
+        {
+            var collection = new List<string>();
+            var readonlyCollection = new ReadOnlyCollection<string>(collection);
+
+            foreach (var element in readonlyCollection)
+            {
+                Assert.Fail("there are no elements in the list yet");
+            }
+
+            collection.Add("first");
+            collection.Add("second");
+            collection.Add("third");
+            collection.Add("fourth");
+
+            using (var collectionEnumerator = collection.GetEnumerator())
+            using (var readonlyCollectionEnumerator = readonlyCollection.GetEnumerator())
+            {
+                while (collectionEnumerator.MoveNext() && readonlyCollectionEnumerator.MoveNext())
+                {
+                    Assert.AreEqual(collectionEnumerator.Current, readonlyCollectionEnumerator.Current);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Ensures that enumerating a readonly collection has the same results as enumerating into its delegate
+        /// </summary>
+        [TestCategory("Unit")]
+        [Priority(2)]
+        [Description("Ensures that enumerating a readonly collection has the same results as enumerating into its delegate")]
+        [TestMethod]
+        public void CollectionEnumeration()
+        {
+            var collection = new List<string>();
+            var readonlyCollection = new ReadOnlyCollection<string>(collection);
+
+            foreach (var element in readonlyCollection)
+            {
+                Assert.Fail("there are no elements in the list yet");
+            }
+
+            collection.Add("first");
+            collection.Add("second");
+            collection.Add("third");
+            collection.Add("fourth");
+
+            var collectionEnumerator = ((IEnumerable)collection).GetEnumerator();
+            var readonlyCollectionEnumerator = ((IEnumerable)readonlyCollection).GetEnumerator();
+            while (collectionEnumerator.MoveNext() && readonlyCollectionEnumerator.MoveNext())
+            {
+                Assert.AreEqual(collectionEnumerator.Current, readonlyCollectionEnumerator.Current);
+            }
+        }
+    }
+}
---
+++ Source/Core/System/Collections/Generic/ReadOnlyCollection.cs
@@ -0,0 +1,61 @@
+﻿namespace System.Collections.Generic
+{
+    using Fx;
+
+    /// <summary>
+    /// An adapter which leverages the data in a <see cref="ICollection{T}"/> to implement a <see cref="IReadOnlyCollection{T}"/>
+    /// </summary>
+    /// <typeparam name="T">They type of the elements stored in this collection</typeparam>
+    /// <threadsafety static="true" instance="false"/>
+    /// <remarks>This type is not immutable</remarks>
+    public sealed class ReadOnlyCollection<T> : IReadOnlyCollection<T>
+    {
+        /// <summary>
+        /// The <see cref="ICollection{T}"/> whose data will be used when we delegate each of our <see cref="IReadOnlyCollection{T}"/> calls to it
+        /// </summary>
+        private readonly ICollection<T> collection;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReadOnlyCollection{T}"/> class
+        /// </summary>
+        /// <param name="collection">
+        /// The <see cref="ICollection{T}"/> that we will delegate each of our <see cref="IReadOnlyCollection{T}"/> calls to and will serve as the data source for our implementation
+        /// </param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="collection"/> is null</exception>
+        public ReadOnlyCollection(ICollection<T> collection)
+        {
+            Ensure.NotNull(collection, nameof(collection));
+
+            this.collection = collection;
+        }
+
+        /// <summary>
+        /// Gets the number of elements in the collection
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return this.collection.Count;
+            }
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection
+        /// </summary>
+        /// <returns>A <see cref="IEnumerator{T}"/> that can be used to iterate through the collection</returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this.collection.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Returns an enumerator that iterates through a collection
+        /// </summary>
+        /// <returns>An <see cref="IEnumerator"/> object that can be used to iterate through the collection</returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this.collection).GetEnumerator();
+        }
+    }
+}
