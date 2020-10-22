c623d28051e5c497fa952ee69aa99338685ce2d9
corranrogue9 <corranrogue9@gmail.com>
2020-10-22 10:18:19 -0700

    added a clock interface and a datetime implementation

---
+++ Source/Core/Fx/Clock/DateTimeClock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fx.Clock
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <threadsafety static="true" instance="true"/>
+    public sealed class DateTimeClock : IClock
+    {
+        private DateTimeClock()
+        {
+        }
+
+        public static DateTimeClock Instance { get; } = new DateTimeClock();
+
+        public DateTime UtcNow
+        {
+            get
+            {
+                return DateTime.UtcNow;
+            }
+        }
+    }
+}
---
+++ Source/Core/Fx/Clock/IClock.cs
@@ -0,0 +1,16 @@
+﻿namespace Fx.Clock
+{
+    using System;
+
+    /// <summary>
+    /// Gets the current time stamp
+    /// </summary>
+    /// <threadsafety instance="true"/>
+    public interface IClock
+    {
+        /// <summary>
+        /// The current time in UTC
+        /// </summary>
+        DateTime UtcNow { get; }
+    }
+}
