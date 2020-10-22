c639a4765479447c3f17082d84c455cc1f9abe9f
corranrogue9 <corranrogue9@gmail.com>
2020-10-22 11:27:29 -0700

    asdf

---
+++ Source/Core/Fx/Clock/ClockExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Fx.Clock
+{
+    using System;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <threadsafety static="true" instance="true"/>
+    public static class ClockExtensions
+    {
+        public static DateTime Today(this IClock clock)
+        {
+            return clock.UtcNow.Date;
+        }
+    }
+}
---
+++ Source/Core/Fx/Clock/DelegateClock.cs
@@ -0,0 +1,32 @@
+﻿namespace Fx.Clock
+{
+    using System;
+    using System.Threading;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <threadsafety static="true" instance="true"/>
+    public sealed class ManualClock : IClock //// TODO need better name
+    {
+        private long current;
+
+        public ManualClock(DateTime start)
+        {
+            this.current = start.Ticks;
+        }
+
+        public DateTime UtcNow
+        {
+            get
+            {
+                return new DateTime(this.current);
+            }
+        }
+
+        public void Increment(TimeSpan delta)
+        {
+            Interlocked.Add(ref this.current, delta.Ticks);
+        }
+    }
+}
