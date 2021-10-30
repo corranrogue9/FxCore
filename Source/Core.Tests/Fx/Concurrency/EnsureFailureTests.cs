136fe25be94f23ef3c72dd582a919f835f850d15
corranrogue9 <corranrogue9@gmail.com>
2021-10-29 23:33:31 -0700

    asef

--- Source/Core.Tests/Fx/Concurrency/EnsureFailureTests.cs
+++ Source/Core.Tests/Fx/Concurrency/EnsureFailureTests.cs
@@ -1,5 +1,7 @@
 ﻿namespace Fx.Concurrency
 {
+    using System;
+    using System.Collections.Concurrent;
     using System.Diagnostics;
     using System.Linq;
     using System.Text;
@@ -41,6 +43,48 @@
 
             var result = output.ToString();
             Trace.Write(result);
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            var queue = new ConcurrentQueue<string>();
+            ////Action action = () => queue.Enqueue("asdf");
+            Func<int> action = () => 1;
+            var bytes = action.Method.GetMethodBody().GetILAsByteArray();
+            var generatedAction = Utilities.GenerateAction(bytes);
+            var generatedBytes = generatedAction.Method.GetMethodBody().GetILAsByteArray();
+            generatedAction();
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var localQueue = new ConcurrentQueue<string>();
+            Func<ConcurrentQueue<string>, int> action = (queue) => new ConcurrentQueue<string>().Count + 3;
+            var bytes = action.Method.GetMethodBody().GetILAsByteArray();
+            var generatedAction = Utilities.GenerateAction2<ConcurrentQueue<string>>(bytes);
+            var result = generatedAction(localQueue);
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            var localQueue = new ConcurrentQueue<string>();
+            Func<string, int> action = (queue) => "asdf"[0] + 3;
+            var bytes = action.Method.GetMethodBody().GetILAsByteArray();
+            var generatedAction = Utilities.GenerateAction2<string>(bytes);
+            var result = generatedAction("asdf");
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            Func<int, int, int> action = (first, second) => first + second + 2;
+            var bytes = action.Method.GetMethodBody().GetILAsByteArray(); //// TODO replacing 0x04 with 0x02 causes this to work correctly...
+            ////bytes = new byte[] { 0x02, 0x03, 0x58, 0x2A };
+            var generatedAction = Utilities.GenerateAction3(bytes);
+            var result = generatedAction(10, 2);
         }
     }
 }
---
+++ Source/Core/Fx/Concurrency/Utilities.cs
@@ -0,0 +1,55 @@
+﻿namespace Fx.Concurrency
+{
+    using System;
+    using System.Reflection;
+    using System.Reflection.Emit;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <threadsafety static="true" instance="true"/>
+    public static class Utilities
+    {
+        public static Action GenerateAction(byte[] bytes)
+        {
+            var asmName = new AssemblyName("gdebruinassembly");
+            var asmBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(asmName, System.Reflection.Emit.AssemblyBuilderAccess.RunAndSave);
+            var module = asmBuilder.DefineDynamicModule("gdebruinmodule");
+            var typeBuilder = module.DefineType("gdebruintype");
+            var method = typeBuilder.DefineMethod("gdebruinmethod", MethodAttributes.Public | MethodAttributes.Static, typeof(int), new Type[0]);
+            method.CreateMethodBody(bytes, bytes.Length);
+            var type = typeBuilder.CreateType();
+            var result = (Func<int>)type.GetMethod("gdebruinmethod").CreateDelegate(typeof(Func<int>));
+            var returned = result();
+            return () => result();
+        }
+
+        public static Func<T, int> GenerateAction2<T>(byte[] bytes)
+        {
+            var asmName = new AssemblyName("gdebruinassembly");
+            var asmBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(asmName, System.Reflection.Emit.AssemblyBuilderAccess.RunAndSave);
+            //// TODO i think you somehow need to add references to the assembly
+            var module = asmBuilder.DefineDynamicModule("gdebruinmodule");
+            var typeBuilder = module.DefineType("gdebruintype");
+            var method = typeBuilder.DefineMethod("gdebruinmethod", MethodAttributes.Public | MethodAttributes.Static, typeof(int), new[] { typeof(T) });
+            method.CreateMethodBody(bytes, bytes.Length);
+            var type = typeBuilder.CreateType();
+            var result = (Func<T, int>)type.GetMethod("gdebruinmethod").CreateDelegate(typeof(Func<T, int>));
+            return result;
+        }
+
+        public static Func<int, int, int> GenerateAction3(byte[] bytes)
+        {
+            var asmName = new AssemblyName("gdebruinassembly");
+            var asmBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(asmName, System.Reflection.Emit.AssemblyBuilderAccess.RunAndSave);
+            //// TODO i think you somehow need to add references to the assembly
+            var module = asmBuilder.DefineDynamicModule("gdebruinmodule");
+            var typeBuilder = module.DefineType("gdebruintype");
+            var method = typeBuilder.DefineMethod("gdebruinmethod", MethodAttributes.Public | MethodAttributes.Static, typeof(int), new[] { typeof(int), typeof(int) });
+            method.CreateMethodBody(bytes, bytes.Length);
+            var type = typeBuilder.CreateType();
+            var result = (Func<int, int, int>)type.GetMethod("gdebruinmethod").CreateDelegate(typeof(Func<int, int, int>));
+            return result;
+        }
+    }
+}
