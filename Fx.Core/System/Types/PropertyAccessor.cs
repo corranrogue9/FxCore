2e949828ae08113882b23e57345da5be49515bef
Garrett DeBruin <corranrogue9@gmail.com>
2023-11-18 14:08:40 -0800

    implementing merge

--- Fx.Core/System/Types/PropertyAccessor.cs
+++ Fx.Core/System/Types/PropertyAccessor.cs
@@ -44,7 +44,75 @@ namespace System.Types
             this IBetterOrderedEnumerable<TSource, TCompare> first,
             IBetterOrderedEnumerable<TSource, TCompare> second)
         {
-            return null;
+            if (first.Selector != second.Selector)
+            {
+                throw new ArgumentException();
+            }
+
+            var comparer = Comparer<TSource>.Default;
+
+            using (var firstEnumerator = first.GetEnumerator())
+            using (var secondEnumerator = second.GetEnumerator())
+            {
+                if (!firstEnumerator.MoveNext())
+                {
+                    if (!secondEnumerator.MoveNext())
+                    {
+                        return Enumerable.Empty<TSource>();
+                    }
+
+                    return secondEnumerator.Enumerate();
+                }
+
+                if (!secondEnumerator.MoveNext())
+                {
+                    return firstEnumerator.Enumerate();
+                }
+
+                return MergeIterator(firstEnumerator, secondEnumerator, comparer);
+            }
+        }
+
+        private static IEnumerable<T> MergeIterator<T>(IEnumerator<T> firstEnumerator, IEnumerator<T> secondEnumerator, IComparer<T> comparer)
+        {
+            var firstElement = firstEnumerator.Current;
+            var secondElement = secondEnumerator.Current;
+            var firstHasMoved = true; // this *has* moved in the caller, hence the default to true
+            var secondHasMoved = true; // this *has* moved in the caller, hence the default to true
+
+            while (firstHasMoved && secondHasMoved)
+            {
+                if (comparer.Compare(firstElement, secondElement) < 0)
+                {
+                    yield return firstElement;
+                    firstHasMoved = firstEnumerator.MoveNext();
+                }
+                else
+                {
+                    yield return secondElement;
+                    secondHasMoved = secondEnumerator.MoveNext();
+                }
+            }
+
+            // *at least* one of firstHasMoved and secondHasMoved is false; they cannot both be true; Enumerate returns the current element, so calling it on an
+            // enumerator that has moved past the end will throw for most enumerators
+            if (firstHasMoved)
+            {
+                firstEnumerator.Enumerate();
+            }
+            else if (secondHasMoved)
+            {
+                secondEnumerator.Enumerate();
+            }
+        }
+
+        private static IEnumerable<T> Enumerate<T>(this IEnumerator<T> enumerator)
+        {
+            yield return enumerator.Current;
+            while (enumerator.MoveNext())
+            {
+                yield return enumerator.Current;
+            }
         }
     }
 
@@ -52,7 +120,30 @@ namespace System.Types
     {
         public static IBetterOrderedEnumerable<TSource, TCompare> BetterOrderBy<TSource, TCompare>(this IEnumerable<TSource> self, Func<TSource, TCompare> selector)
         {
-            return null;
+            return new BetterOrderedEnumerable<TSource, TCompare>(self, selector);
+        }
+    }
+
+    public class BetterOrderedEnumerable<TSource, TCompare> : IBetterOrderedEnumerable<TSource, TCompare>
+    {
+        private readonly IEnumerable<TSource> source;
+
+        public BetterOrderedEnumerable(IEnumerable<TSource> source, Func<TSource, TCompare> selector)
+        {
+            this.source = source;
+            this.Selector = selector;
+        }
+
+        public Func<TSource, TCompare> Selector { get; }
+
+        public IEnumerator<TSource> GetEnumerator()
+        {
+            return this.source.OrderBy(this.Selector).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
         }
     }
 
